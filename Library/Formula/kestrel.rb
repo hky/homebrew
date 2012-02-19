ca107badbd60ebc2e9662671ae0002c13ac097ee
Adam Vandenberg <flangy@gmail.com>
2012-02-19 10:37:27 -0800

    wip

--- Library/Formula/kestrel.rb
+++ Library/Formula/kestrel.rb
@@ -1,41 +1,35 @@
 require 'formula'
 
 class Kestrel < Formula
-  url 'http://robey.github.com/kestrel/download/kestrel-2.1.4.zip'
   homepage 'http://robey.github.com/kestrel/'
-  md5 '0859860deaf31e4031bfdaf618c812aa'
+  url 'http://robey.github.com/kestrel/download/kestrel-2.1.5.zip'
+  md5 '256503b15fb7feec37e100f5ef92f94d'
 
   depends_on 'daemon'
 
-  def binfile
-    return <<-EOBINFILE
-#!/bin/sh
-exec #{prefix}/scripts/kestrel.sh "$@"
-EOBINFILE
-  end
-
   def install
-
     inreplace 'scripts/kestrel.sh' do |s|
-        s.gsub!(/\/usr\/local\/\$APP_NAME\/current/, "#{prefix}")
-        s.gsub!(/\/var\/log\/\$APP_NAME/, "#{var}/log/$APP_NAME")
-        s.gsub!(/\/var\/run\/\$APP_NAME/, "#{var}/run/$APP_NAME")
-        s.gsub!(/\/etc\/init.d\/\$\{APP_NAME\}.sh/, "$0")
+      s.change_make_var! "APP_HOME", libexec
+      s.gsub! "/var/log/$APP_NAME/", "#{var}/log/$APP_NAME"
+      s.gsub! "/var/run/$APP_NAME/", "#{var}/run/$APP_NAME"
+      # Fix path to script in help message
+      s.gsub! "Usage: /etc/init.d/${APP_NAME}.sh", "Usage: kestrel"
     end
 
-    prefix.install Dir['*']
-    (prefix+'scripts/kestrel.sh').chmod 0755
-    (prefix+'scripts/devel.sh').chmod 0755
+    libexec.install Dir['*']
+    (libexec+'scripts/kestrel.sh').chmod 0755
+    (libexec+'scripts/devel.sh').chmod 0755
 
     (var+'log/kestrel').mkpath
     (var+'run/kestrel').mkpath
 
-    (bin).mkpath
-    (bin+'kestrel').write binfile
-    (bin+'kestrel').chmod 0755
+    (bin+'kestrel').write <<-EOS.undent
+      #!/bin/bash
+      exec "#{libexec}/scripts/kestrel.sh" "$@"
+    EOS
   end
 
   def test
-      system "#{bin}/kestrel status"
+    system "#{bin}/kestrel status"
   end
 end
