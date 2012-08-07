307c251431622d0125f224b06bf4ab22ada1180e
Adam Vandenberg <flangy@gmail.com>
2012-08-07 11:27:56 -0700

    use shim

--- Library/Formula/app-engine-java-sdk.rb
+++ Library/Formula/app-engine-java-sdk.rb
@@ -5,20 +5,12 @@ class AppEngineJavaSdk < Formula
   url 'http://googleappengine.googlecode.com/files/appengine-java-sdk-1.6.6.zip'
   sha1 'bb67c8984606fc8d28ab4f49afc088b5b70d9097'
 
-  def shim_script target
-    <<-EOS.undent
-      #!/bin/bash
-      "#{libexec}/bin/#{target}" "$@"
-    EOS
-  end
-
   def install
     rm Dir['bin/*.cmd']
     libexec.install Dir['*']
 
-    Dir["#{libexec}/bin/*"].each do |b|
-      n = Pathname.new(b).basename
-      (bin+n).write shim_script(n)
+    Dir["#{libexec}/bin/*"].each do |n|
+      bin.write_exec_script n
     end
   end
 end
